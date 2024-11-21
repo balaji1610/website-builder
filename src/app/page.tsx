7800c0ba18e3a6dce54e3849bdd4c87d0e054bee
Balaji <81224567+balaji1610@users.noreply.github.com>
2024-11-21 17:34:43 +0530

    implemented mobile screen
    WB-39

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 "use client";
 
 import Login from "./components/login";
+import useMediaQuery from "@mui/material/useMediaQuery";
 export default function Home() {
-  return (
-    <>
-      <Login />
-    </>
-  );
+  const ismobileDevice = useMediaQuery("(max-width:600px)");
+  return <>{ismobileDevice ? <h1>Available on desktop only</h1> : <Login />}</>;
 }
